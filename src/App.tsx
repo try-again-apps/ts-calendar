8821fbae21f37cd0b3ebad663054b35c42338aa6
try-again-apps <try.again.apps@gmail.com>
2018-12-22 00:03:38 +0100

    Add material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 import logo from './logo.svg';
 
@@ -14,6 +15,7 @@ class App extends React.Component {
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Button variant="contained">Test material UI</Button>
       </div>
     );
   }
